eb603ce6817c6c0a915ce26baa625c7ddb260c90
jsuitters <jsuitters@gmail.com>
2021-02-07 14:54:56 +0000

    Updated readme to include info on csv file update

--- tests.py
+++ tests.py
@@ -5,13 +5,5 @@ from random import randint
 def random_test(grades):
     for val in grades.values():
         val[1] = randint(1, 4)
+    print(grades)
     return grades
-
-'''
-# randomise grade scores for testing | paste into main.py when required
-# remember to import test.py into main.py
-stg2_grades = random_test(stg2_grades)
-print(stg2_grades)
-stg3_grades = random_test(stg3_grades)
-print(stg3_grades)
-'''
