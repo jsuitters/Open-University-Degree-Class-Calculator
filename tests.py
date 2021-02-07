792a5784c96d986439e598fc24098cea7efd388d
jsuitters <jsuitters@gmail.com>
2021-02-07 14:08:00 +0000

    Added csv file to pull grades in from

--- tests.py
+++ tests.py
@@ -6,3 +6,12 @@ def random_test(grades):
     for val in grades.values():
         val[1] = randint(1, 4)
     return grades
+
+'''
+# randomise grade scores for testing | paste into main.py when required
+# remember to import test.py into main.py
+stg2_grades = random_test(stg2_grades)
+print(stg2_grades)
+stg3_grades = random_test(stg3_grades)
+print(stg3_grades)
+'''
